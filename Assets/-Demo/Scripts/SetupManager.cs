de2d5d8e2d8fb0e654f7a42029d3b3e15d388a8b
Newman Willis <newmanw@sturfee.com>
2019-09-04 15:38:30 -0700

    Basic flying car setup complete

---
+++ Assets/-Demo/Scripts/SetupManager.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Sturfee.Unity.XR.Core.Events;
+using UnityEngine;
+
+public class SetupManager : MonoBehaviour {
+
+    public GameObject Car1;
+    public GameObject Car2;
+
+    [Header("Testing")]
+    bool StartOnSessionReady;
+
+    private void Awake()
+    {
+        Car1.SetActive(false);
+        Car2.SetActive(false);
+    }
+
+    void Start () {
+        SturfeeEventManager.Instance.OnSessionReady += OnSessionReady;         SturfeeEventManager.Instance.OnLocalizationSuccessful += OnLocalizationSuccessful;
+        //Hashtable test;
+        //test
+    }
+
+    #region EventFunctions
+    private void OnLocalizationSuccessful()
+    {
+        StartCoroutine(StartAr());
+    }
+
+    private void OnSessionReady()
+    {
+        if (StartOnSessionReady)
+        {
+            StartCoroutine(StartAr());
+        }
+    }
+    #endregion
+
+    private IEnumerator StartAr()
+    {
+        Car1.SetActive(true);
+        yield return new WaitForSeconds(6);
+        Car2.SetActive(true);
+    }
+
+}
