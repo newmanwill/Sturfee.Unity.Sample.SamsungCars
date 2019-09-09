90613c518028fbe534b4b865e51afc6628155bb5
Newman Willis <newmanw@sturfee.com>
2019-09-09 16:21:25 -0700

    Added updated Samsung Office scene

---
+++ Assets/-Demo/Scripts/SetupManagerSMO.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Sturfee.Unity.XR.Core.Events;
+using UnityEngine;
+
+public class SetupManagerSMO : MonoBehaviour {
+
+    //public GameObject Car1;
+    //public GameObject Car2;
+
+    public GameObject[] Cars;
+
+    [Header("Testing")]
+    public bool StartOnSessionReady;
+
+    private void Awake()
+    {
+        SetCars(false);
+        //Car1.SetActive(false);
+        //Car2.SetActive(false);
+    }
+
+    void Start () {
+        SturfeeEventManager.Instance.OnSessionReady += OnSessionReady;         SturfeeEventManager.Instance.OnLocalizationSuccessful += OnLocalizationSuccessful;
+    }
+
+    #region EventFunctions
+    private void OnLocalizationSuccessful()
+    {
+        StartCoroutine(StartAr());
+    }
+
+    private void OnSessionReady()
+    {
+        if (StartOnSessionReady)
+        {
+            StartCoroutine(StartAr());
+        }
+    }
+    #endregion
+
+    private IEnumerator StartAr()
+    {
+        yield return null;
+        SetCars(true);
+        //Car1.SetActive(true);
+        ////yield return new WaitForSeconds(6);
+        //Car2.SetActive(true);
+    }
+
+    private void SetCars(bool val)
+    {
+        for(int i = 0; i < Cars.Length; i++)
+        {
+            Cars[i].SetActive(val);
+        }
+    }
+
+
+
+}
